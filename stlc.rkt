405cf7ebda74affe6606c3a5dfc3bba8429a999f
Xu Xue <juniorxxue@gmail.com>
2020-11-25 22:31:59 +0800

    appsub relation

--- stlc.rkt
+++ stlc.rkt
@@ -1,22 +1,31 @@
 #lang racket
 (require redex)
 (define-language AppL
-  (e ::=
+  (e ::= ;; term
      n ;; int
      x ;; variable
      (λ x e) ;; abstraction
      (e e) ;; application
      (e ◇ e) ;; merge
-     (x τ) ;; annotation
+     (x : A) ;; annotation
      )
   (n ::=
      number)
-  (A B C D ::=
+  (A B C D ::= ;; type
      Int ;; int type
+     Bool ;; bool type
      Top ;; top type
      (A → B) ;; function type
      (A & B) ;; intersection type
      )
+  (T ::= ;; context
+     Empty ;; empty
+     (T x : A)) ;; bind (x : A) in T
+
+  (S ::= ;; arguments given
+     Empty ;; empty
+     (S : A)) ;; with argument S : A
+
   (x ::= variable-not-otherwise-mentioned))
 
 (define-judgment-form AppL
@@ -40,5 +49,33 @@
   [(subtype B C)
    --------------------- sub_andr
    (subtype (A & B) C)])
-   
-     
+
+(test-equal (judgment-holds (subtype Int Top)) #t)
+(test-equal (judgment-holds (subtype (Top → Top) (Int → Top))) #t)
+
+(define-judgment-form AppL
+  #:mode (appsub I I I)
+  #:contract (appsub S A B)
+  [-------------------- appsub_refl
+   (appsub Empty A A)]
+  [(subtype C A)
+   (appsub S B D)
+   ---------------------------- appsub_fun
+   (appsub (S : C) (A → B) D)]
+  [(appsub (S : C) A D)
+   ---------------------------- appsub_andl
+   (appsub (S : C) (A & B) D)]
+  [(appsub (S : C) B D)
+   ---------------------------- appsub_andr
+   (appsub (S : C) (A & B) D)]
+  [(appsub (S : A) C D)
+   --------------------------- appsub_and1
+   (appsub (S : (A & B)) C D)]
+  [(appsub (S : B) C D)
+   --------------------------- appsub_and2
+   (appsub (S : (A & B)) C D)]
+  )
+
+(test-equal (judgment-holds (appsub Empty Int Int)) #t)
+(test-equal (judgment-holds (appsub (Empty : Int) (Int → Int) Int)) #t)
+(test-equal (judgment-holds (appsub (Empty : Int) ((Int → Int) & (Bool → Bool)) Int)) #t)
