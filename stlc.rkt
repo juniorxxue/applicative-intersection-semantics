7399991be2055b6dd92daacbef489ae9489cf426
Xu Xue <juniorxxue@gmail.com>
2020-11-30 23:00:28 +0800

    rewrite test cases and refactor symbols

--- stlc.rkt
+++ stlc.rkt
@@ -19,63 +19,66 @@
      (A & B) ;; intersection type
      )
   (T ::= ;; context
-     Empty ;; empty
+     · ;; empty
      (T x : A)) ;; bind (x : A) in T
 
   (S ::= ;; arguments given
-     Empty ;; empty
+     · ;; empty
      (S : A)) ;; with argument S : A
 
   (x ::= variable-not-otherwise-mentioned))
 
 (define-judgment-form AppL
-  #:mode (subtype I I)
-  #:contract (subtype A B)
+  #:mode (subtype I I I)
+  #:contract (subtype A <: B)
   [--------------- sub_int
-   (subtype Int Int)]
+   (subtype Int <: Int)]
   [--------------- sub_top
-   (subtype A Top)]
-  [(subtype C A)
-   (subtype B D)
+   (subtype A <: Top)]
+  [(subtype C <: A)
+   (subtype B <: D)
    ------------------------ sub_arrow
-   (subtype (A → B) (C → D))]
-  [(subtype A B)
-   (subtype A C)
+   (subtype (A → B) <: (C → D))]
+  [(subtype A <: B)
+   (subtype A <: C)
    --------------------- sub_and
-   (subtype A (B & C))]
-  [(subtype A C)
+   (subtype A <: (B & C))]
+  [(subtype A <: C)
    --------------------- sub_andl
-   (subtype (A & B) C)]
-  [(subtype B C)
+   (subtype (A & B) <: C)]
+  [(subtype B <: C)
    --------------------- sub_andr
-   (subtype (A & B) C)])
+   (subtype (A & B) <: C)])
 
-(test-equal (judgment-holds (subtype Int Top)) #t)
-(test-equal (judgment-holds (subtype (Top → Top) (Int → Top))) #t)
+(test-equal (judgment-holds (subtype Int <: Top)) #t)
+(test-equal (judgment-holds (subtype (Top → Top) <: (Int → Top))) #t)
 
 (define-judgment-form AppL
-  #:mode (appsub I I I)
-  #:contract (appsub S A B)
+  #:mode (appsub I I I I O)
+  #:contract (appsub S ⊢ A <: B)
   [-------------------- appsub_refl
-   (appsub Empty A A)]
-  [(subtype C A)
-   (appsub S B D)
+   (appsub · ⊢ A <: A)]
+  [(subtype C <: A)
+   (appsub S ⊢ B <: D)
    ---------------------------- appsub_fun
-   (appsub (S : C) (A → B) D)]
-  [(appsub (S : C) A D)
+   (appsub (S : C) ⊢ (A → B) <: D)]
+  [(appsub (S : C) ⊢ A <: D)
    ---------------------------- appsub_andl
-   (appsub (S : C) (A & B) D)]
-  [(appsub (S : C) B D)
+   (appsub (S : C) ⊢ (A & B) <: D)]
+  [(appsub (S : C) ⊢ B <: D)
    ---------------------------- appsub_andr
-   (appsub (S : C) (A & B) D)]
-  [(appsub (S : A) C D)
+   (appsub (S : C) ⊢ (A & B) <: D)]
+  [(appsub (S : A) ⊢ C <: D)
    --------------------------- appsub_and1
-   (appsub (S : (A & B)) C D)]
-  [(appsub (S : B) C D)
+   (appsub (S : (A & B)) ⊢ C <: D)]
+  [(appsub (S : B) ⊢ C <: D)
    --------------------------- appsub_and2
-   (appsub (S : (A & B)) C D)]
+   (appsub (S : (A & B)) ⊢ C <: D)]
   )
 
-(test-equal (judgment-holds (appsub Empty Int Int)) #t)
-(test-equal (judgment-holds (appsub (Empty : Int) (Int → Int) Int)) #t)
-(test-equal (judgment-holds (appsub (Empty : Int) ((Int → Int) & (Bool → Bool)) Int)) #t)
+(test-equal (judgment-holds (appsub · ⊢ Int <: A) A)
+            (list (term Int)))
+(test-equal (judgment-holds (appsub (· : Int) ⊢ (Int → Int) <: A) A)
+            (list (term Int)))
+(test-equal (judgment-holds (appsub (· : Int) ⊢ ((Int → Int) & (Bool → Bool)) <: A) A)
+            (list (term Int)))
